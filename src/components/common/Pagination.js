7acf7ecbd73897937e2283aec31d3589ce8f7ef5
Kelly Carter <kelly@rainworksweb.com>
2017-09-12 21:25:59 -0700

    Improved pagination UI for large datasets

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -7,41 +7,44 @@ const Pagination = props => {
   const totalNumPages = Math.floor( props.count / props.perPage );
   
   const onNextClick = e => {
+    e.preventDefault();
     props.onClick(e, props.page + 1);
   };
   
   const onPreviousClick = e => {
+    e.preventDefault();
     props.onClick(e, props.page - 1);
   };
 
   const onPageClick = e => {
-    props.onClick(e, e.currentTarget.innerText - 1);
+    props.onClick(e, parseInt(e.currentTarget.value));
   };
 
   let rows = [];
   for (let i = 0; i <= totalNumPages; i++) {
-    rows.push((
-      <li key={i} className={`page-item ${props.page == i && 'active'}`}>
-        <PaginationLink onClick={onPageClick}>{i + 1}</PaginationLink>
-      </li>
-    ));
+    rows.push(<option key={i} value={i}>{i + 1}</option>);
   }
 
   return (
-    <nav className="row" aria-label="Search results navigation">
-      <div className="col-3">
+    <form className="row" aria-label="Search results navigation">
+      <div className="col-4 pt-2">
         Page {props.page + 1} of {totalNumPages + 1}
       </div>
-      <ul className="col-9 justify-content-end pagination">
-        <li className={`page-item ${props.page == 0 && "disabled"}`}>
-          <PaginationLink onClick={onPreviousClick}><Icon className="fa-arrow-left" /></PaginationLink>
-        </li>
-        {rows}
-        <li className={`page-item ${props.page == totalNumPages && "disabled"}`}>
-          <PaginationLink onClick={onNextClick}><Icon className="fa-arrow-right" /></PaginationLink>
-        </li>
-      </ul>
-    </nav>
+      <div className="col-4" />
+      <div className="col-4 mb-3">
+        <div className="input-group input-group-sm">
+          <span className="input-group-btn">
+            <PaginationLink onClick={onPreviousClick} disabled={props.page == 0}><Icon className="fa-arrow-left" /></PaginationLink>
+          </span>
+          <select className="form-control custom-select" value={props.page} onChange={onPageClick}>
+            {rows}
+          </select>
+          <span className="input-group-btn">
+            <PaginationLink onClick={onNextClick} disabled={props.page == totalNumPages}><Icon className="fa-arrow-right" /></PaginationLink>
+          </span>
+        </div>
+      </div>
+    </form>
   );
 };
 
--- src/components/common/PaginationLink.js
+++ src/components/common/PaginationLink.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 
 const PaginationLink = props => {
     return (
-        <button className="page-link" onClick={props.onClick}>{props.children}</button>
+        <button className="btn btn-default" onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
     );
 };
 
 PaginationLink.propTypes = {
     children: PropTypes.node,
+    disabled: PropTypes.bool.isRequired,
     onClick: PropTypes.func
+};
+
+PaginationLink.defaultProps = {
+    disabled: false
 };
 
 export default PaginationLink;
